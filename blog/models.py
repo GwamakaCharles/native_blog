b4867dd73a88cef0616ec9be638b5c6326181533
Gwamaka Charles <gwaxart19@gmail.com>
2019-07-01 11:00:38 +0300

    Added the comment approve or disapprove ability to the authenticated users only!

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,10 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
